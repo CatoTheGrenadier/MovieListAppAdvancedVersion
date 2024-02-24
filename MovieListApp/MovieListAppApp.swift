4fb452f9a71fce75ff8fedc7d8e2f68335be58d4
Cato the Chevalier <131333594+CatoTheGrenadier@users.noreply.github.com>
2024-02-24 14:47:29 -0500

    Initial Commit

---
+++ MovieListApp/MovieListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieListAppApp.swift
+//  MovieListApp
+//
+//  Created by Yi Ling on 2/24/24.
+//
+
+import SwiftUI
+
+@main
+struct MovieListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
